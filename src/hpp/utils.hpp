fc6fc02b250aeffb73114a2d7cecb469ff076957
Miguel Costa <miguelccs_@outlook.com>
2025-12-01 10:04:36 -0300

    implementacao header de funcoes utilitarias

---
+++ src/hpp/utils.hpp
@@ -0,0 +1,23 @@
+/*
+utils.hpp
+Funcoes utilitarias: RNG e iniciativa
+*/
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include "lista.hpp"
+#include <string>
+
+
+void inicializarRNG();
+
+int rolarDado(int tipoDado);
+
+int calcularIniciativa(int resultadoDado, int modificador);
+
+void rolarIniciativaPersonagem(Personagem* p);
+
+void rolarIniciativaTodos(Lista* lista);
+
+#endif
